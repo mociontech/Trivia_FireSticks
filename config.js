6447cdc15574921ed19daae656930f4f3078970e
mociontech <112130191+mociontech@users.noreply.github.com>
2026-05-13 20:58:09 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+window.TRIVIA_DATAHUB_CONFIG = {
+  enabled: true,
+  baseUrl: "https://mocion-dev-62y2v.ondigitalocean.app/evius/api/datahub",
+  eventId: "e4af4785-0321-470d-a6e0-5cbb1bd145bd",
+  experienceId: "4e5adb1e-016b-424c-94bf-30971eeb5d27",
+  token:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImMzODVmNWZlLTY1ZDktNDlmZS04MWI0LTc0YWMxYmZmYjhkNCIsInR5cGUiOiJhZG1pbiIsInJvbCI6InN1cGVyX2FkbWluIiwiaXNBZG1pbiI6dHJ1ZSwidG9rZW5WZXJzaW9uIjowLCJpYXQiOjE3Nzg3MDk3MDksImV4cCI6MTc3ODc1MjkwOX0.gTZqcHEYkYdzHVFPoTD2M4CBXJa1Sc3Pvi0ABMIYTbs",
+  source: "trivia-firesticks",
+};
